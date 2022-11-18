edae95d4dce2a324604e9a970abe621a864997a8
BANCHA WAIPEA <banchawaiwai4@gmail.com>
2022-11-18 14:35:51 +0700

    update

--- app/models.py
+++ app/models.py
@@ -7,9 +7,11 @@ class Teacher(models.Model):
     name = models.CharField(max_length = 150)
     first_name = models.CharField(max_length = 150)
     last_name = models.CharField(max_length = 150)
+    avatar = models.ImageField(upload_to='avatar', null=True, blank=True)
 
     def __str__(self):
         return '%s %s %s' % (self.name, self.first_name, self.last_name)
+
 
 class Branch(models.Model):
     name = models.CharField(max_length = 150)
@@ -23,19 +25,21 @@ class Branch(models.Model):
 # path ForeignKey
 
 class Paper(models.Model):
-    title = models.CharField(max_length = 255)
-    abstract_thai = models.TextField()
-    abstract_eng = models.TextField()
-    link_paper = models.CharField(max_length = 255)
-    keyword = models.CharField(max_length = 150)
-    upload_files = models.FileField(upload_to='file', null=True, blank=True)
-    teachers = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    branchs = models.ForeignKey(Branch, on_delete=models.CASCADE)
-    date_paper = models.DateField()
-    approve = models.BooleanField(default=False)
+    title_th = models.CharField(max_length = 255) # หัวข้อวิจัย ภาษาไทย
+    title_eg = models.CharField(max_length = 255, null=True, blank=True) # หัวข้อวิจัย ภาษาอังกฤษ 
+    researcher = models.CharField(max_length = 150) # ผู้วิจัย
+    abstract_thai = models.TextField(blank=True)# บทนำ ภาษาไทย
+    abstract_eng = models.TextField(blank=True)# บทนำ ภาษาอังกฤษ
+    link_paper = models.URLField(max_length = 200, blank=True)# link paper
+    keyword = models.CharField(max_length = 150, blank=True) # คำสำคัญ
+    upload_files = models.FileField(upload_to='file', null=True, blank=True) # uppload file paper
+    teachers = models.ForeignKey(Teacher, on_delete=models.CASCADE) # อาจารย์ที่ปรึกษา
+    branchs = models.ForeignKey(Branch, on_delete=models.CASCADE) # สาขาวิชา
+    date_paper = models.DateField() # วันเดือนปี เอกสาร
+    approve = models.BooleanField(default=False) # อนุมัติ Paper
 
     def __str__(self):
-        return self.title
+        return self.title_th
 
 
     
--- app/views.py
+++ app/views.py
@@ -1,8 +1,14 @@
 from unicodedata import name
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Paper
 
 def Index(request):
-
-    return render(request, 'main.html')
+    data_papers = Paper.objects.all()
+    context = {
+        'data_papers':data_papers
+    }
+    # context['data_paper'] = Paper.objects.all()
+    # Add in a QuerySet of all the books
+    # context['paper_list'] = Paper.objects.all()
+    return render(request, 'main.html', context)
