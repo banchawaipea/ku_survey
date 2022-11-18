25cfbd18fb84b96602042ce0634190cbf6cfd940
BANCHA WAIPEA <banchawaiwai4@gmail.com>
2022-11-18 15:05:00 +0700

    update

--- project/settings.py
+++ project/settings.py
@@ -136,12 +136,14 @@ STATIC_URL = 'static/'
 
 # เพิ่มใหม่
 STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
-STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
+# For DEV
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles') 
+# For Deploy
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 #เพิ่มใหม่
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
