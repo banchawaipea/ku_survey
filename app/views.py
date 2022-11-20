67cadea4eab5dd0ff004f9e5edec11564403cd11
BANCHA WAIPEA <banchawaiwai4@gmail.com>
2022-11-21 00:16:36 +0700

    up

--- app/views.py
+++ app/views.py
@@ -8,6 +8,9 @@ def Index(request):
     
     data_papers = Paper.objects.all().order_by('-id') # ดึงข้อมูลจาก model มาแสดง
 
+    paginator = Paginator(data_papers, 10) # แบ่งหน้าข้อมูล
+    page = paginator.get_page(1) # ดึงหน้าที่เลือกมาแสดง
+    
     # context['data_paper'] = Paper.objects.all()
     # Add in a QuerySet of all the books
     # context['paper_list'] = Paper.objects.all()
